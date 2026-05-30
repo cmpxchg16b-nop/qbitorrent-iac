aad844b7b652b26213cdf2058c245c2a5506ffa3
root <root@wien1.exploro.one>
2026-05-30 22:35:20 +0000

    Update init script

--- init.d/01-setup-host-nw.sh
+++ init.d/01-setup-host-nw.sh
@@ -6,3 +6,19 @@ ip -n qb l set eth0 up
 
 ip a add 10.169.254.2/30 dev v-qb
 ip -n qb a add 10.169.254.1/30 dev eth0
+
+ip -6 a add fd96:4158:7963::2/64 dev v-qb
+ip -n qb -6 a add fd96:4158:7963::1/64 dev eth0
+
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
+nft add table ip qb-nat
+nft add chain ip qb-nat postrouting '{ type nat hook postrouting priority srcnat ; policy accept ; }'
+nft add rule ip qb-nat postrouting ip saddr 10.169.254.0/30 oifname "v-qb" accept
+nft add rule ip qb-nat postrouting ip saddr 10.169.254.0/30 masquerade
+
+nft add table ip6 qb-nat6
+nft add chain ip6 qb-nat6 postrouting '{ type nat hook postrouting priority srcnat ; policy accept ; }'
+nft add rule ip6 qb-nat6 postrouting ip6 saddr fd96:4158:7963::/64 oifname "v-qb" accept
+nft add rule ip6 qb-nat6 postrouting ip6 saddr fd96:4158:7963::/64 masquerade
